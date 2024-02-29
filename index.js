c80c0a7109951cd4618a455e00fbe02142c0e8eb
vimal-eshwaran <118733978+vimal-eshwaran@users.noreply.github.com>
2024-02-29 19:38:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
-import axios from 'axios'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import Reducers from './reducers'
 
-const API = axios.create({ baseURL: "https://syackoverflow-backend-x7qg.onrender.com"})
+const store = createStore( Reducers, compose(applyMiddleware(thunk)))
 
-API.interceptors.request.use((req) => {
-    if(localStorage.getItem('Profile')){
-        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('Profile')).token}`
-    }
-    return req;
-})
-
-export const logIn = (authData) => API.post('/user/login', authData);
-export const signUp = (authData) => API.post('/user/signup', authData);
-
-export const postQuestion = (questionData) => API.post('/questions/Ask', questionData)
-export const getAllQuestions = () => API.get('/questions/get');
-export const deleteQuestion = (id) => API.delete(`/questions/delete/${id}`) 
-export const voteQuestion = (id, value ) => API.patch(`/questions/vote/${id}`, { value })
-
-export const postAnswer = (id, noOfAnswers, answerBody, userAnswered ) => API.patch(`/answer/post/${id}`, { noOfAnswers, answerBody, userAnswered })
-export const deleteAnswer = (id, answerId, noOfAnswers) => API.patch(`/answer/delete/${id}`, { answerId, noOfAnswers})
-
-export const getAllUsers = () => API.get('/user/getAllUsers');
-export const updateProfile = (id, updateData) => API.patch(`/user/update/${id}`, updateData)
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
+);
